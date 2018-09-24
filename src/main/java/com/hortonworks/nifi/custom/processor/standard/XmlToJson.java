18b8597e11ed7163f3d8f6e1aa09738aec526b4d
Sachin Kekatpuray <skekatpuray@HW13124.local>
2018-09-24 08:32:00 -0400

    Checking in

--- src/main/java/com/hortonworks/nifi/custom/processor/standard/XmlToJson.java
+++ src/main/java/com/hortonworks/nifi/custom/processor/standard/XmlToJson.java
@@ -32,6 +32,13 @@ import org.json.XML;
 @CapabilityDescription("Converts Xml to JSON")
 public class XmlToJson extends AbstractProcessor {
 
+    static final PropertyDescriptor SomeProperty = new PropertyDescriptor.Builder()
+            .name("SomeProperty")
+            .displayName("Some Property")
+            .description("Get some value")
+            .required(true)
+            .build();
+
     static final Relationship REL_SUCCESS = new Relationship.Builder()
             .name("success")
             .description("Succes relationship")
@@ -45,7 +52,7 @@ public class XmlToJson extends AbstractProcessor {
     @Override
     protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
         final List<PropertyDescriptor> properties = new ArrayList<>();
-
+        properties.add(SomeProperty);
         return properties;
     }
 
@@ -62,9 +69,7 @@ public class XmlToJson extends AbstractProcessor {
     @Override
     public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
 
-
         final AtomicReference<String> value = new AtomicReference<>();
-
 
         FlowFile flowFile = session.get();
         if (flowFile == null) {
@@ -72,6 +77,8 @@ public class XmlToJson extends AbstractProcessor {
         }
 
         try {
+
+            String someProperty = context.getProperty(SomeProperty).getValue().toString();
 
             session.read(flowFile, new InputStreamCallback() {
                 @Override
